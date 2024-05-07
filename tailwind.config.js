e7304a98db60d0e3d8b1e6cd0cd13c05fa7554b0
Jonah Setiawan <114904513+iHanzyy@users.noreply.github.com>
2024-05-07 21:48:40 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#1153ed",
+        secondary: "#2a54b5",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
